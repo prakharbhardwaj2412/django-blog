2a745ca31b2f5d29435c20a4b182a8976db3462d
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2021-04-03 08:05:31 +0530

    user profile

--- Tensor_Blog/Tensor_Blog/settings.py
+++ Tensor_Blog/Tensor_Blog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# MEDIA_ROOT is where our uploaded file will be uploaded
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
